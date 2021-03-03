55b6545e701c2d45b8e1485f92dbd244d48f5fa9
FMinister <56680320+FMinister@users.noreply.github.com>
2021-03-03 18:38:50 +0100

    + blog & blog_test

--- tests/unit/post_test.py
+++ tests/unit/post_test.py
@@ -4,7 +4,13 @@ from post import Post
 
 class PostTest(TestCase):
     def test_create_post(self):
-        p = Post('Test', 'Test Content')
+        p = Post("Test", "Test Content")
 
-        self.assertEqual('Test', p.title)
-        self.assertEqual('Test Content', p.content)
+        self.assertEqual("Test", p.title)
+        self.assertEqual("Test Content", p.content)
+
+    def test_json(self):
+        p = Post("Test", "Test Content")
+        expected = {"title": "Test", "content": "Test Content"}
+
+        self.assertDictEqual(expected, p.json())
